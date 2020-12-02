7ff34c82b6a32815ea843cd5f100e0b1110ccec2
Ayoub Faragi <afaragi@e1r9p6.1337.ma>
2020-12-02 02:08:40 +0100

    edit sun/moon bg on js

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,5 @@
 var all = document.getElementById("all").style;
-var but = document.getElementById("toggle");
 var pos = document.getElementById("ball").style;
-console.log(pos);
 window.onload = getcookie();
 function setcookie(filter) {
   document.cookie = "value=" + filter;
@@ -9,26 +7,26 @@ function setcookie(filter) {
 function getcookie() {
   var value = document.cookie.split("=");
   if (value[1] === "") {
-    but.setAttribute("value", "Dark");
-    pos.setProperty("left", "0");
+    pos.setProperty("left", "0",);
+    pos.setProperty("background-image", "url(./img/sun.png)",);
     all.filter = "";
     setcookie(all.filter);
   } else if (value[1] === "invert(1) hue-rotate(180deg)") {
-    but.setAttribute("value", "Light");
     pos.setProperty("left", "55px");
+    pos.setProperty("background-image", "url(./img/moon.png)",);
     all.filter = "invert(1) hue-rotate(180deg)";
     setcookie(all.filter);
   }
 }
 function darkmode() {
   if (all.filter === "") {
-    but.setAttribute("value", "Light");
     pos.setProperty("left", "55px");
+    pos.setProperty("background-image", "url(./img/moon.png)",);
     all.filter = "invert(1) hue-rotate(180deg)";
     setcookie(all.filter);
   } else if (all.filter === "invert(1) hue-rotate(180deg)") {
-    but.setAttribute("value", "Dark");
     pos.setProperty("left", "0");
+    pos.setProperty("background-image", "url(./img/sun.png)",);
     all.filter = "";
     setcookie(all.filter);
   }
